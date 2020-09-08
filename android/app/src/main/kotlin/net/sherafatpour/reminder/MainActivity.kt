8777360752991b5d7691445cff554c8dc4700318
Reza sherafatpour <reza.sharafatpour@gmail.com>
2020-09-08 11:29:38 +0430

    reminder

---
+++ android/app/src/main/kotlin/net/sherafatpour/reminder/MainActivity.kt
@@ -0,0 +1,6 @@
+package net.sherafatpour.reminder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF2D2F41),
+
+    );
+  }
+}
